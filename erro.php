706c586844e6f4a4f687fb64ad860cb3d686cce6
Sérgio Tripeça <35767050+sergiotripeca@users.noreply.github.com>
2018-02-15 12:41:17 +0000

    imagem
    so mudei a imagem

--- erro.php
+++ erro.php
@@ -1,5 +1,5 @@
 <?php session_start();
 	echo'erro';
-	echo'<body https://www.1and1.com/digitalguide/fileadmin/DigitalGuide/Teaser/not-found-t.jpg>';
+	echo'<body https://png.clipart.me/previews/5de/404-error-page-47090.jpg>';
 	echo'<form method="post" action="Login.html"><input name="Voltar" type="submit" value="Voltar"></form>';
 ?>
